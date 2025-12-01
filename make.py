d2bb3f517930aa04f148878bd0f4bbb5845153e0
StufflyerScripting <167232463+StufflyerScripting@users.noreply.github.com>
2025-12-01 19:11:19 +0100

    Added macOs compilation support
    Edited compilation command

--- make.py
+++ make.py
@@ -3,8 +3,11 @@ import sys
 
 
 if sys.platform == "win32":
-  os.system("g++ ./src/main.cpp -o main.exe")
+    os.system("g++ -shared -o ./std/stdlib.dll ./std/stdlib.cpp")
 elif sys.platform == "linux":
-  os.system("g++ ./src/main.cpp -o main.o")
+    os.system("g++ -fPIC -shared -o ./std/stdlib.so ./std/stdlib.cpp")
+elif sys.platform == "darwin":
+    os.system("g++ -dynamiclib -o ./std/stdlib.dylib ./std/stdlib.cpp")
 else:
     print("OS NOT SUPPORTED")
+os.system("g++ main.cpp -o main.o")
