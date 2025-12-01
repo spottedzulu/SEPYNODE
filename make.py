d7a27ea79b79b3136a56d2171b9ac55244d61fd3
StufflyerScripting <167232463+StufflyerScripting@users.noreply.github.com>
2025-12-01 19:17:05 +0100

    Update make.py

--- make.py
+++ make.py
@@ -1,13 +1,17 @@
 import os
 import sys
 
-
 if sys.platform == "win32":
     os.system("g++ -shared -o ./std/stdlib.dll ./std/stdlib.cpp")
+    os.system("g++ .src/main.cpp -o main.exe")
+
 elif sys.platform == "linux":
     os.system("g++ -fPIC -shared -o ./std/stdlib.so ./std/stdlib.cpp")
+    os.system("g++ .src/main.cpp -o main") 
+
 elif sys.platform == "darwin":
     os.system("g++ -dynamiclib -o ./std/stdlib.dylib ./std/stdlib.cpp")
+    os.system("g++ .src/main.cpp -o main")
+
 else:
     print("OS NOT SUPPORTED")
-os.system("g++ main.cpp -o main.o")
