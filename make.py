083cd2fc9230b49b7f348574816477194e69067f
spottedzulu <spottedzulu@gmail.com>
2026-03-02 23:00:37 +0200

    Comment out shared library build commands
    Comment out the commands for building shared libraries on different platforms.

--- make.py
+++ make.py
@@ -2,16 +2,16 @@ import os
 import sys
 
 if sys.platform == "win32":
-    os.system("g++ -shared -o ./std/stdlib.dll ./std/stdlib.cpp")
+    //os.system("g++ -shared -o ./std/stdlib.dll ./std/stdlib.cpp")
     os.system("g++ .src/main.cpp -o main.exe")
 
 elif sys.platform == "linux":
-    os.system("g++ -fPIC -shared -o ./std/stdlib.so ./std/stdlib.cpp")
+    //os.system("g++ -fPIC -shared -o ./std/stdlib.so ./std/stdlib.cpp")
     os.system("g++ .src/main.cpp -o main") 
 
 elif sys.platform == "darwin":
-    os.system("g++ -dynamiclib -o ./std/stdlib.dylib ./std/stdlib.cpp")
-    os.system("g++ .src/main.cpp -o main")
+    //os.system("g++ -dynamiclib -o ./std/stdlib.dylib ./std/stdlib.cpp")
+    os.system("g++ .src/main.cpp -o main")// to be honest I don't think I added dlib support so it will probably crash lol
 
 else:
     print("OS NOT SUPPORTED")
